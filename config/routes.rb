b261353f1c7ab2fbd2bff1b8079b7de8fd9c9e5b
0sugo <josugo26@gmail.com>
2023-08-15 14:23:05 +0300

    Fix rubocop offences

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get 'random_greeting', to:'messages#random_greeting'
+      get 'random_greeting', to: 'messages#random_greeting'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_14_223019) do
-  create_table "messages", force: :cascade do |t|
-    t.string "greeting"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_230_814_223_019) do
+  create_table 'messages', force: :cascade do |t|
+    t.string 'greeting'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
